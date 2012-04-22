2fcaf2ac6f545d50bd11e59314cb3d0eb32a3bf1
Markiyan Matsekh <mr.matsekh@gmail.com>
2012-04-22 16:34:15 +0300

    added Sticks game with N max sticks to take

--- GameTheory/GameTheory.SpragueGrundy/Program.cs
+++ GameTheory/GameTheory.SpragueGrundy/Program.cs
@@ -12,12 +12,21 @@ namespace GameTheory.SpragueGrundy
         {
             var lasker = new LaskersNim();
             var kayles = new KaylesGame();
+
+            var sticks2 = new SticksGame(2);
+            var sticks3 = new SticksGame(3);
+            var sticks4 = new SticksGame(4);
+
             for (uint i = 0; i < 100; i++)
             {
                 var l = lasker.Grundy(i);
                 var k = kayles.Grundy(i);
 
-                Console.WriteLine("n: {0}; \tlasker: {1}; \tkayles: {2}", i, l, k);
+                var s2 = sticks2.Grundy(i);
+                var s3 = sticks3.Grundy(i);
+                var s4 = sticks4.Grundy(i);
+
+                Console.WriteLine("n: {0}; \tlasker: {1}; \tkayles: {2}; \tsticks(2,3,4): {3},{4},{5}", i, l, k, s2, s3, s4);
             }
 
         }

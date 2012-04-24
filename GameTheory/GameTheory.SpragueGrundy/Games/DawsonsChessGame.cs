a4b4772d81b0390c73ec41767da94e4e8b9a8592
Markiyan Matsekh <mr.matsekh@gmail.com>
2012-04-24 19:05:53 +0300

    fixed Dawsons Chess to work correctly

--- GameTheory/GameTheory.SpragueGrundy/Games/DawsonsChessGame.cs
+++ GameTheory/GameTheory.SpragueGrundy/Games/DawsonsChessGame.cs
@@ -1,14 +1,77 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using GameTheory.SpragueGrundy.Helpers;
 
 namespace GameTheory.SpragueGrundy.Games
 {
-    // winning strategy is to always try to make nim-sum of the rest of piles == 0 (that is - to make them equal, in case of 2 piles);
-    public class DawsonsChessGame : GrundyGameBase<List<int>>
+    public class PileList : List<int>, IEquatable<PileList>
     {
-        protected override bool TryStopRecursion(List<int> key, out uint value)
+        public PileList(IEnumerable<int> collection)
+            : base(collection)
         {
-            if (key.IsEmpty())
+        }
+
+        public PileList()
+        {
+        }
+
+        public bool Equals(PileList other)
+        {
+            if (this.Count != other.Count)
+                return false;
+
+            //this.Sort(); // very rude, redo this
+            //other.Sort();
+
+            for (int i = 0; i < Count; i++)
+                if (this[i] != other[i])
+                    return false;
+
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != typeof(PileList)) return false;
+            return Equals((PileList)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            var res = this.Aggregate(0, (current, chip) => current ^ chip);
+            return res;
+        }
+
+        public static bool operator ==(PileList left, PileList right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(PileList left, PileList right)
+        {
+            return !Equals(left, right);
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("Count = ").Append(Count).Append(";Piles sizes: ");
+            foreach (var chip in this)
+                sb.Append(chip).Append(";");
+            return sb.ToString();
+        }
+    }
+
+    // winning strategy is to always try to make nim-sum of the rest of piles == 0 (that is - to make them equal, in case of 2 piles);
+    public class DawsonsChessGame : GrundyGameBase<PileList>
+    {
+        protected override bool TryStopRecursion(PileList key, out uint value)
+        {
+            if (key.IsEmpty() || key.All(chip => chip == 0))
             {
                 value = 0;
                 return true;
@@ -18,56 +81,65 @@ namespace GameTheory.SpragueGrundy.Games
             return false;
         }
 
-        public override HashSet<List<int>> GetStateTransitions2(List<int> key)
+        public override HashSet<PileList> GetStateTransitions2(PileList key)
         {
-            var set = new HashSet<List<int>>();
+            var set = new HashSet<PileList>();
 
             // remove 1 chip if it's a whole pile
             for (int i = 0; i < key.Count; i++)
             {
                 if (key[i] == 1)
                 {
-                    var list = new List<int>(key);
+                    var list = new PileList(key);
                     list.RemoveAt(i);
-                    if (!list.IsEmpty())
-                        set.Add(list);
+                    set.Add(list);
                 }
             }
 
             // remove 2 chips from any pile
             for (int i = 0; i < key.Count; i++)
             {
-                var list = new List<int>(key);
+                var list = new PileList(key);
                 list[i] -= 2;
                 if (list[i] <= 0)
                     list.RemoveAt(i);
-                if (!list.IsEmpty())
-                    set.Add(list);
+                set.Add(list);
+
             }
 
             // remove 3 chips and split this pile on 2, 1 or 0 (depending on situation)
             for (int i = 0; i < key.Count; i++)
             {
-                var list = new List<int>(key);
+                var list = new PileList(key);
                 list[i] -= 3;
 
                 int leftChipsCount = list[i];
-                list.RemoveAt(i);
-
                 int middleOfChips = leftChipsCount / 2 + 1;
 
-                // handle corner case
-                if (leftChipsCount > 0)
-                    set.Add(new List<int>(list) {leftChipsCount});
+                list.RemoveAt(i);
+
+                if (leftChipsCount == 0)
+                {
+                    if (!list.IsEmpty())
+                        set.Add(new PileList(list));
+                    else
+                        set.Add(new PileList(list) { leftChipsCount });
+                }
+                else if (leftChipsCount > 0)
+                    set.Add(new PileList(list) { leftChipsCount });
 
                 for (int j = 1; j < middleOfChips; j++)
-                    set.Add(new List<int>(list) {j, leftChipsCount - j});
+                    set.Add(new PileList(list) { j, leftChipsCount - j });
             }
+
+            // little cleanup - theoretically, zeros dont affect xor-sum, but they affect
+           // foreach (var pile in set)
+                //pile.Remove(0);
 
             return set;
         }
 
-        protected override HashSet<uint> GetStateTransitions(List<int> key)
+        protected override HashSet<uint> GetStateTransitions(PileList key)
         {
             var spSet = new HashSet<uint>();
             foreach (var list in GetStateTransitions2(key))
--- GameTheory/GameTheory.SpragueGrundy/Program.cs
+++ GameTheory/GameTheory.SpragueGrundy/Program.cs
@@ -12,56 +12,13 @@ namespace GameTheory.SpragueGrundy
     {
         static void Main(string[] args)
         {
-            var lasker = new LaskersNimGame();
-            var kayles = new KaylesGame();
-
-            var sub2 = new SubstractionGame(new HashSet<uint> { 1, 2 });
-            var sub3 = new SubstractionGame(new HashSet<uint> { 1, 2, 3 });
-            var sub2N = new SubstractionGame(new HashSet<uint> { 1, 2, 4, 8, 16 });
-
-            //Console.WriteLine(ResultPredictor.FindPNPositions(sub2, 40).BinaryView);
-            //Console.WriteLine(ResultPredictor.FindPNPositions(sub3, 40).BinaryView);
-            //Console.WriteLine(ResultPredictor.FindPNPositions(sub2N, 40).BinaryView);
-            //Console.WriteLine(ResultPredictor.FindPNPositions(lasker, 40).BinaryView);
-
-            //Console.WriteLine(ResultPredictor.PredictWinner<uint>(sub2N, 100, Player.First));
-
-
-            var knight = new WhiteKnightGame();
-            var queen = new QueenGame();
-            var chomp = new ChompGame(10,10);
-
-            //int n = 10;
-            //for (int i = 1; i <= n; i++)
-            //{
-            //    for (int j = 1; j <= n; j++)
-            //    {
-            //        var sg = chomp.SGValue(new Coordinate(i, j));
-            //        var binary = sg == 0 ? 0 : 1;
-            //        Console.Write("{0}", sg);
-            //    }
-            //    Console.WriteLine();
-            //}
-
-            //var a = new XorEquation(4, 7, 9);
-            //var b = a.Sum();
-            //var zeros = a.FindZeroResults();
-            //foreach (var zero in zeros)
-            //{
-            //    var d = a.DoReplacement(zero);
-            //    var e = d.Sum();
-            //}
 
             var dawson = new DawsonsChessGame();
-            var a = dawson.GetStateTransitions2(new List<int>() {2,9});
-
-            var b = dawson.SGValue(new List<int>() {5});
-
-            foreach (var set in a)
+            for (int i = 0; i < 19; i++)
             {
-                var c = new XorEquation(set);
-                var d = c.FindZeroResults();
+                Console.WriteLine("{0}-{1}",i,dawson.SGValue(new PileList() {i}));
             }
+
         }
     }
 }
